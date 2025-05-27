e9a04c13c25774b036461b39958e8ccef3cd2a76
Rasheed Ram <42969080+cruelwalker@users.noreply.github.com>
2025-05-27 13:50:04 +0200

    shopify cleint in infra

---
+++ domains/insights/filters/discount_code_metrics.py
@@ -0,0 +1,3 @@
+import shopify
+
+shop_url = ""
--- infra/clients/shopify.py
+++ infra/clients/shopify.py
@@ -1,3 +1,19 @@
+import os
+import requests
+
+shop_name= os.getenv("shopify_shop_name")
+access_token = os.getenv("shopify_access_token")
+api_version = os.getenv("shopify_api_v", "2025-04")
+url = f"https://{shop_name}.myshopify.com/admin/api/{api_version}/"
+
+headers = {
+    "X-Shopify-Access-Token": access_token,
+    "Content-Type": "application/json"
+}
+
+def get_shopify_response():
+    return requests.get(url, headers=headers)
+
 def retrieve_shopify_data():
     """
     Function to retrieve data from Shopify.
