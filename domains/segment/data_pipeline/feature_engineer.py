cb68e4979bfffb6db25e8de9dffb8ed5c5d7762f
Rasheed Ram <42969080+cruelwalker@users.noreply.github.com>
2025-05-31 21:33:51 +0200

    list sku _ has discount, drop unfeatured and not needed columns

--- domains/segment/data_pipeline/feature_engineer.py
+++ domains/segment/data_pipeline/feature_engineer.py
@@ -4,19 +4,20 @@ from pandas import DataFrame
 class feature_engineering_segmentation:
     df : DataFrame
     df_payment_dummies : DataFrame
+
     def __init__(self):
         #self.file_path = "Data\\Processed Data\\orders_filled.csv"
 
         pass
     def order_features(self):
-        self.df['Nb Orders'] = df.groupby('Email')['Id'].transform('nunique')
-        self.df['Amount Orders'] = df.groupby('Email')['Total'].transform('sum')
-        self.df['Avg Amount Orders'] = round(df.groupby('Email')['Total'].transform('mean'),2)
-        self.df['Max Amount Orders'] = df.groupby('Email')['Total'].transform('max')
-        self.df['Nb items'] = df.groupby('Email')['Lineitem quantity'].transform('sum')
-        self.df['Avg Nb items'] = round(df.groupby('Email')['Lineitem quantity'].transform('mean'),0)
-        self.df['Avg item amount'] = round(df.groupby('Email')['Lineitem price'].transform('mean'),2)
-        self.df['Max item amount'] = df.groupby('Email')['Lineitem price'].transform('max')
+        self.df['Nb Orders'] = self.df.groupby('Email')['Id'].transform('nunique')
+        self.df['Amount Orders'] = self.df.groupby('Email')['Total'].transform('sum')
+        self.df['Avg Amount Orders'] = round(self.df.groupby('Email')['Total'].transform('mean'),2)
+        self.df['Max Amount Orders'] = self.df.groupby('Email')['Total'].transform('max')
+        self.df['Nb items'] = self.df.groupby('Email')['Lineitem quantity'].transform('sum')
+        self.df['Avg Nb items'] = round(self.df.groupby('Email')['Lineitem quantity'].transform('mean'),0)
+        self.df['Avg item amount'] = round(self.df.groupby('Email')['Lineitem price'].transform('mean'),2)
+        self.df['Max item amount'] = self.df.groupby('Email')['Lineitem price'].transform('max')
     
     def categorize_payment(method): 
         if 'ideal' in method.lower():
@@ -48,7 +49,7 @@ class feature_engineering_segmentation:
     def payment_dummy_vars(self):
         self.df_payment_dummies = pd.get_dummies(self.df['Payment Method New'], prefix='PayMeth').astype(int)
         # Merge with the original DataFrame
-        self.df = pd.concat([self.df, df_payment_dummies], axis=1)
+        self.df = pd.concat([self.df, self.df_payment_dummies], axis=1)
 
     def group_by_email(self):
         df_payment_dummies_grouped = self.df.groupby('Email')[self.df_payment_dummies.columns].max().reset_index()
@@ -63,4 +64,40 @@ class feature_engineering_segmentation:
         # Merge back to the original dataframe to assign recent values
         self.df = self.df.merge(latest_info, on='Email', how='left')
     
+    def has_discount_column(self):
+        self.df['Has Discount'] = self.np.where(self.df['Discount Amount'] > 0, 1, 0)
+        # Condition 1: Total is 0 OR Condition 2: Total > Discount Amount, # Assign 1 if either condition is met, # Otherwise, compute the discount percentage
+        self.df['Discount Per'] = self.np.where((self.df['Total'] == 0) | (self.df['Total'] < self.df['Discount Amount']), 1, round(self.df['Discount Amount'] / self.df['Total'],2))
+        self.df['Free Shipping'] = self.np.where(self.df['Shipping'] == 0, 1, 0)
+        self.df['Always Discount'] = self.df.groupby('Email')['Has Discount'].transform(lambda x: int((x == 1).all()))
+        self.df['Always Free Shipping'] = self.df.groupby('Email')['Free Shipping'].transform(lambda x: int((x == 1).all()))
+        self.df['Never Discount'] = self.df.groupby('Email')['Has Discount'].transform(lambda x: int((x == 0).all()))
+        self.df['Never Free Shipping'] = self.df.groupby('Email')['Free Shipping'].transform(lambda x: int((x == 0).all()))
+        self.df['Max Discount Percentage'] = self.df.groupby('Email')['Discount Per'].transform('max')
+
+    def sku_list(self):
+        df_SKU = self.df.groupby('Email', as_index=False).agg({'Lineitem sku': list})
+        df_SKU.rename(columns={'Lineitem sku': 'List SKU'}, inplace=True)
+        #check if sku apear more than once
+        df_SKU['Same SKU more than once'] = df_SKU['List SKU'].apply(lambda x: any(x.count(sku) > 1 for sku in set(x)))
+        df_SKU['Same SKU more than once'] = df_SKU['Same SKU more than once'].astype(int)
+        #merge with original 
+        self.df = self.df.merge(df_SKU, on='Email', how='left')
+    
+    def drop_columns(self):
+        # Drop to columns
+        drop1 = ['Payment Method', 'Lineitem quantity', 'Lineitem name', 'Lineitem price', 'Lineitem sku']
+        self.df = self.df.drop(columns=drop1)
+        drop2 = ['Name', 'Paid at', 'Accepts Marketing', 'Shipping', 'Total', 'Discount Amount', 'Billing City','Billing Country','Id', 'Days Since today', 'Payment Method New', 'Has Discount', 'Discount Per','Free Shipping']
+        self.df = self.df.drop(columns=drop2)
+
+    def group_by_email(self):
+        self.df = self.df.groupby('Email', as_index=False).first()
+
+    def top_10_countries(self):
+        pass
+    def top_10_cities(self):
+        pass
+    def top_10_skus(self):
+        pass
     
