44275be6b9331792a89bf8f37c29edd404c28d75
Rasheed Ram <42969080+cruelwalker@users.noreply.github.com>
2025-06-07 20:07:16 +0200

    441 accu

--- domains/segment/dbscan_model.py
+++ domains/segment/dbscan_model.py
@@ -30,7 +30,7 @@ class dbscan_model:
             "DaysSinceRecentOrder", "Nb Orders",
             "Nb items", "PayMeth_Bancontact", "PayMeth_Card", "PayMeth_Ideal",
             "PayMeth_Klarna", "PayMeth_Other", "PayMeth_Pay Later", "PayMeth_shopify payments",
-            "Always Discount", "Always Free Shipping", "Never Discount", "Max Discount Percentage",
+            "Always Discount", "Always Free Shipping", "Max Discount Percentage",
             "Same SKU more than once", "Email Marketing Consent", "Accepts Marketing",
             "click", "open", "Days since First Active", "Recent City_amersfoort",
             "Recent City_amsterdam", "Recent City_den haag", "Recent City_rotterdam",
